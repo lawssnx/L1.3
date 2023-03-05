cae928156fc43a17d0cc97a4ca507a963d75821b
anastasi <120751391+lawssnx@users.noreply.github.com>
2023-03-05 11:31:06 +0200

    Update L1.3.cs

--- L1.3.cs
+++ L1.3.cs
@@ -1,8 +1,21 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public class Class1
+class Program
 {
-	public Class1()
-	{
-	}
+    static void Main(string[]args)
+    {
+        Console.Write("Введіть цілі числа через пробіл: ");
+        List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        var featured = numbers.Where(x => x % 2 != 0)
+            .OrderBy(x => x);
+        Console.Write("Непарні числа у порядку зростання:");
+
+        foreach (int s in featured)
+        {
+            Console.WriteLine(s);
+        }
+
+    }
 }
